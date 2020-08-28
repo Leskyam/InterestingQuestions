772e2d8f7705031dd2f400687d9a3f8b3058eb72
Lesky Alfonso Martinez <leskyam@gmail.com>
2020-08-28 04:54:34 -0500

    Add project files.

---
+++ InterestingQuestions.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterestingQuestions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
